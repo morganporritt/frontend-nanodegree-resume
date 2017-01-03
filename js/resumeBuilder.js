88b4452a97f8ead627b5334f5b59d09e77f1bd24
Morgan Porritt <mporritt@Morgans-iMac.local>
2017-01-03 16:43:22 -0700

    Continued working on building each element.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,9 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-
-
 var bio = {
 	"name": "Morgan Porritt",
 	"role": "Software Developer",
 	"contacts": {
 		"mobile": "123-123-1234",
-		"email": "test@test.com",
+		"email": "morganporritt@gmail.com",
 		"github": "morganporritt",
 		"twitter": "NA",
 		"location": "Logan UT"
@@ -19,18 +14,18 @@ var bio = {
 	"display": "diplayBio()"
 };
 
-var education {
+var education = {
 	"schools": [{
 		"name": "Utah State University",
 		"location": "Logan UT",
-		"degree": "Bachelors",
-		"majors": ["Computer Science"],
+		"degree": "BA",
+		"majors": ["Computer Science", "Business"],
 		"dates": "2010-2015",
 		"url": "http://www.usu.edu"
 	}, {
 		"name": "Bear River High School",
 		"location": "Garland UT",
-		"degree": "High School Diploma",
+		"degree": "Diploma",
 		"majors": ["NA"],
 		"dates": "2004-2007",
 		"url": "http://www.brhs.besd.net/"
@@ -49,7 +44,7 @@ var education {
 	"display": "displayEducation()"
 };
 
-var work {
+var work = {
 	"jobs": [{
 		"employer": "Autoliv",
 		"title": "Intern",
@@ -72,7 +67,7 @@ var work {
 	"display": "displayJobs()"
 };
 
-var projects {
+var projects = {
 	"projects": [{
 		"title": "Project 1",
 		"dates": "2010-2014",
@@ -94,13 +89,43 @@ var projects {
 };
 
 
+// Header/Bio Section
+
+bio.skills.forEach(function (e) {
+    $("#header").prepend(HTMLskills.replace("%data%", e));
+});
+
+$("#header").prepend(HTMLskillsStart);
+$("#header").prepend(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
 $("#header").prepend(HTMLbioPic.replace("%data%", bio.biopic));
 $("#header").prepend(HTMLlocation.replace("%data%", bio.contacts.location));
 $("#header").prepend(HTMLgithub.replace("%data%", bio.contacts.github));
 $("#header").prepend(HTMLtwitter.replace("%data%", bio.contacts.twitter));
 $("#header").prepend(HTMLemail.replace("%data%", bio.contacts.email));
 $("#header").prepend(HTMLmobile.replace("%data%", bio.contacts.mobile));
-//$("#header").prepend(HTMLcontactGeneric);
 $("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
 $("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+
+
+// Education Section
+
+$("#education").append(HTMLschoolStart);
+education.schools.forEach(function (e) {
+    var buildName = HTMLschoolName.replace("%data%", e.name).replace("%link%", e.url) + HTMLschoolDegree.replace('%data%', e.degree);
+    $("#education").append(buildName);
+    $("#education").append(HTMLschoolDates.replace("%data%", e.dates));
+    $("#education").append(HTMLschoolLocation.replace("%data%", e.location));
+   
+    e.majors.forEach(function (m) {
+        $("#education").append(HTMLschoolMajor.replace("%data%", m));
+    });
+});
+
+$("#education").append(HTMLonlineClasses);
+education.onlineCourses.forEach(function (e) {
+    var buildName = HTMLonlineTitle.replace("%data%", e.title).replace("%link%", e.url) + HTMLonlineSchool.replace('%data%', e.school);
+    $("#education").append(buildName);
+    $("#education").append(HTMLonlineDates.replace("%data%", e.dates));
+    $("#education").append(HTMLonlineURL.replace("%data%", e.url));
+});
 
